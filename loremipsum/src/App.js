1e09f00c318b9b9b8d104a4ed7781ff4592f0f56
oliver <olivercastroteran@gmail.com>
2020-11-02 19:36:28 -0400

    loremipsum project added

--- loremipsum/src/App.js
+++ loremipsum/src/App.js
@@ -1,8 +1,46 @@
+import { useState } from 'react';
+import data from './data';
+
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = +count;
+
+    if (count <= 0) {
+      amount = 1;
+    } else if (count > 8) {
+      amount = 8;
+    }
+
+    setText(data.slice(0, amount));
+  };
+
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <section className="section-center">
+      <h3>Lorem Ipsum Plus</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className="btn" type="submit">
+          Generate
+        </button>
+      </form>
+
+      <article className="lorem--text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
   );
 }
 
