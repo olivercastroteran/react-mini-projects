c926ee9602bd377e0d67169ea7377205d4cf2f0e
oliver <olivercastroteran@gmail.com>
2020-11-17 10:59:02 -0400

    stripe menu project added

--- stripe-menu/src/App.js
+++ stripe-menu/src/App.js
@@ -1,8 +1,17 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Sidebar from './Sidebar';
+import Submenu from './Submenu';
+
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <>
+      <Navbar />
+      <Sidebar />
+      <Hero />
+      <Submenu />
+    </>
   );
 }
 
