1c77c72df36da5ecd7b46b0254d984bc8c93b724
oliver <olivercastroteran@gmail.com>
2020-11-21 05:30:53 -0400

    cart project added

--- cart/src/App.js
+++ cart/src/App.js
@@ -1,8 +1,25 @@
+import React from 'react';
+import { useGlobalContext } from './context';
+
+// components
+import Navbar from './Navbar';
+import CartContainer from './CartContainer';
+// items
+
 function App() {
+  const { loading } = useGlobalContext();
+  if (loading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
   );
 }
 
---
+++ cart/src/reducer.js
@@ -0,0 +1,82 @@
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'CLEAR_CART':
+      return {
+        ...state,
+        cart: [],
+      };
+    case 'REMOVE':
+      return {
+        ...state,
+        cart: state.cart.filter((item) => item.id !== action.payload),
+      };
+    case 'INCREASE':
+      let tempCart = state.cart.map((item) => {
+        if (item.id === action.payload) {
+          return {
+            ...item,
+            amount: item.amount + 1,
+          };
+        }
+        return item;
+      });
+
+      return {
+        ...state,
+        cart: tempCart,
+      };
+    case 'DECREASE':
+      let newCart = state.cart
+        .map((item) => {
+          if (item.id === action.payload) {
+            return {
+              ...item,
+              amount: item.amount - 1,
+            };
+          }
+          return item;
+        })
+        .filter((item) => item.amount !== 0);
+
+      return {
+        ...state,
+        cart: newCart,
+      };
+    case 'GET_TOTALS':
+      let { total, amount } = state.cart.reduce(
+        (cartTotal, cartItem) => {
+          const { price, amount } = cartItem;
+          const itemTotal = price * amount;
+          cartTotal.amount += amount;
+          cartTotal.total += itemTotal;
+          return cartTotal;
+        },
+        {
+          total: 0,
+          amount: 0,
+        }
+      );
+      total = parseFloat(total.toFixed(2));
+
+      return {
+        ...state,
+        total,
+        amount,
+      };
+    case 'LOADING':
+      return {
+        ...state,
+        loading: true,
+      };
+    case 'DISPLAY_ITEMS':
+      return {
+        ...state,
+        cart: action.payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
